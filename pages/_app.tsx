7aceed4b479e09c8b827b5ac55cca5b19865e039
gavrilciunterei <gavriltaller@gmail.com>
2022-05-06 14:08:34 +0200

    feat: dark theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextUIProvider } from '@nextui-org/react';
+import { dark } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={dark}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
