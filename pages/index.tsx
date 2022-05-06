52581e2820845ef24a12b04d28a2ce8b94833edf
gavrilciunterei <gavriltaller@gmail.com>
2022-05-06 14:03:00 +0200

    feat: added nextUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Hola !</h1>
+      <Button color={'gradient'}>Hola!</Button>
     </>
   );
 };
