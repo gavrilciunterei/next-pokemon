cd001a671afb993ea11131f03e04a57eab68bf21
gavrilciunterei <gavriltaller@gmail.com>
2022-05-09 15:47:39 +0200

    feat: favorites page

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Layout } from '../../components/layouts';
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <h1>Holass</h1>
+    </Layout>
+  );
+};
+
+export default Favorites;
