c3cc9cff4aae4c24b2ddf69497c51207d4024ae7
gavrilciunterei <gavriltaller@gmail.com>
2022-05-09 15:35:02 +0200

    feat: pokemon card and static paths

--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { SmallPokemon } from '../../interfaces';
 import { Card, Grid, Row, Text } from '@nextui-org/react';
+import { useRouter } from 'next/router';
 
 interface Props {
   pokemon: SmallPokemon;
@@ -8,9 +9,15 @@ interface Props {
 
 export const PokemonCard: FC<Props> = ({ pokemon }) => {
   const { id, name, img } = pokemon;
+  const router = useRouter();
+
+  const handleOnClick = () => {
+    router.push(`/pokemon/${id}`);
+  };
+
   return (
     <Grid xs={6} sm={3} md={2} xl={1} key={id}>
-      <Card hoverable clickable>
+      <Card hoverable clickable onClick={handleOnClick}>
         <Card.Body css={{ p: 1 }}>
           <Card.Image src={img} width="100%" height={'140px'} />
         </Card.Body>
